d142302f8a0e486a8f3be80181d2d8c4ffe987f8
luissilvajj <luisxsilva56@gmail.com>
2026-03-09 23:26:10 -0400

    style: change global identity color palette to yellow/amber

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,18 +17,18 @@ module.exports = {
             colors: {
                 // Semantic Palette
                 primary: {
-                    DEFAULT: '#4F46E5', // Indigo-600
-                    50: '#eef2ff',
-                    100: '#e0e7ff',
-                    200: '#c7d2fe',
-                    300: '#a5b4fc',
-                    400: '#818cf8',
-                    500: '#6366f1',
-                    600: '#4f46e5',
-                    700: '#4338ca',
-                    800: '#3730a3',
-                    900: '#312e81',
-                    950: '#1e1b4b',
+                    DEFAULT: '#eab308', // Yellow-500
+                    50: '#fefce8',
+                    100: '#fef9c3',
+                    200: '#fef08a',
+                    300: '#fde047',
+                    400: '#facc15',
+                    500: '#eab308',
+                    600: '#ca8a04',
+                    700: '#a16207',
+                    800: '#854d0e',
+                    900: '#713f12',
+                    950: '#422006',
                 },
                 surface: {
                     ground: 'var(--color-surface-ground)',
